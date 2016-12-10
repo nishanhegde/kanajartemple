8d499bf973cce1dfd2819095cfa34fe6505a4816
C5230125 <C5230125@AGRN50946219A.apj.global.corp.sap>
2016-12-10 17:46:15 +0530

    Email with VM template

--- kanajartemple/src/main/java/com/Admin/listner/NithyaPoojeApprovalListener.java
+++ kanajartemple/src/main/java/com/Admin/listner/NithyaPoojeApprovalListener.java
@@ -1,5 +1,18 @@
 package com.Admin.listner;
 
+import java.io.StringWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.mail.internet.MimeMessage;
+
+import org.apache.log4j.Logger;
+import org.apache.velocity.Template;
+import org.apache.velocity.VelocityContext;
+import org.apache.velocity.app.VelocityEngine;
+import org.apache.velocity.exception.MethodInvocationException;
+import org.apache.velocity.exception.ParseErrorException;
+import org.apache.velocity.exception.ResourceNotFoundException;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -7,29 +20,68 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.ApplicationEvent;
 import org.springframework.context.ApplicationListener;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Component;
+import org.springframework.ui.velocity.VelocityEngineUtils;
 
 import com.Admin.Service.EmailService;
 import com.Admin.Service.kanajarTempleMethods;
+import com.Admin.Service.Impl.AdminHomeService;
+import com.Admin.bean.SashwathaPoojebean;
 import com.Admin.event.NithyaPoojeApprovalEvent;
 
 @Component
 
 public class NithyaPoojeApprovalListener implements ApplicationListener<NithyaPoojeApprovalEvent> {
 
-	@Autowired
-	private EmailService emailService;
+	private static final Logger logger = Logger.getLogger(NithyaPoojeApprovalListener.class);
+
+	@Value("${email.from}")
+	private String from;
+	
+	@Value("${website.url}")
+	private String website;
+	
 
 	@Autowired
+	private JavaMailSender mailSender;
+	
+	@Autowired
 	private ApplicationContext applicationContext;
+
+	@Autowired
+	private VelocityEngine velocityEngine;
 
 	@Override
 	public void onApplicationEvent(NithyaPoojeApprovalEvent event) {
 
 		if (applicationContext.getParent() != null) {
 
-			System.out.println("Approve event" + event.getSource() + "=" + event.getClass());
-
+			try {
+				final SashwathaPoojebean sashwathaPooje = event.getSashwathaPoojebean();
+			
+				 MimeMessagePreparator preparator = new MimeMessagePreparator() {
+                     public void prepare(MimeMessage mimeMessage) throws Exception {
+                             MimeMessageHelper message = new MimeMessageHelper(mimeMessage);
+                             message.setTo(sashwathaPooje.getEmail());
+                             message.setFrom(from); 
+                             message.setSubject("Nithya Pooje Information");
+                             Map model = new HashMap();
+                             model.put("pooje", sashwathaPooje);
+                             model.put("url", website+"/nithyapooje");
+                             String text = VelocityEngineUtils.mergeTemplateIntoString(
+                                             velocityEngine, "./EmailTemplates/nithyapoojeapproval.vm", model);
+                             message.setText(text, true);
+                     }
+             };
+             mailSender.send( preparator);
+			} catch (Exception e) {
+				logger.error(e);
+			}
 		}
 	}
 
--- kanajartemple/src/main/java/com/Admin/listner/NithyaPoojeRejectListener.java
+++ kanajartemple/src/main/java/com/Admin/listner/NithyaPoojeRejectListener.java
@@ -1,32 +1,72 @@
 package com.Admin.listner;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.mail.internet.MimeMessage;
+
+import org.apache.log4j.Logger;
+import org.apache.velocity.app.VelocityEngine;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.ApplicationListener;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Component;
+import org.springframework.ui.velocity.VelocityEngineUtils;
 
 import com.Admin.Service.EmailService;
 import com.Admin.Service.kanajarTempleMethods;
+import com.Admin.bean.SashwathaPoojebean;
 import com.Admin.event.NithyaPoojeRejectEvent;
 
 @Component
 public class NithyaPoojeRejectListener implements ApplicationListener<NithyaPoojeRejectEvent> {
 
-	@Autowired
-	private EmailService emailService;
+	private static final Logger logger = Logger.getLogger(NithyaPoojeRejectListener.class);
 
+	@Value("${email.from}")
+	private String from;
+	
+	@Autowired
+	private JavaMailSender mailSender;
+	
 	@Autowired
 	private ApplicationContext applicationContext;
 
+	@Autowired
+	private VelocityEngine velocityEngine;
+
+
 	@Override
-	public void onApplicationEvent(NithyaPoojeRejectEvent paramE) {
+	public void onApplicationEvent(NithyaPoojeRejectEvent event) {
 		
 		if (applicationContext.getParent()!=null) {
 
-			System.out.println("Reject event");
+			try {
+				final SashwathaPoojebean sashwathaPooje = event.getSashwathaPoojebean();
+			
+				 MimeMessagePreparator preparator = new MimeMessagePreparator() {
+                     public void prepare(MimeMessage mimeMessage) throws Exception {
+                             MimeMessageHelper message = new MimeMessageHelper(mimeMessage);
+                             message.setTo(sashwathaPooje.getEmail());
+                             message.setFrom(from); 
+                             message.setSubject("Nithya Pooje Information");
+                             Map model = new HashMap();
+                             model.put("pooje", sashwathaPooje);
+                             String text = VelocityEngineUtils.mergeTemplateIntoString(
+                                             velocityEngine, "./EmailTemplates/nithyapoojereject.vm", model);
+                             message.setText(text, true);
+                     }
+             };
+             mailSender.send( preparator);
+			} catch (Exception e) {
+				logger.error(e);
+			}
 		}
 	}
 }
